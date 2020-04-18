f1787440990ae28f1e322b544b00abfd19761fe7
Derek Myers <arcticpro@gmail.com>
2020-04-18 03:11:07 -0500

    facade

---
+++ src/Facade.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NotificationChannels\Phoney;
+
+use Illuminate\Support\Facades\Facade as FacadeProvider;
+
+class Facade extends FacadeProvider
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'phoney';
+    }
+}
