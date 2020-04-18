d193ac370a6532020847188e4e9a76d42802899c
Derek Myers <arcticpro@gmail.com>
2020-04-18 03:04:18 -0500

    cleanup

---
+++ src/PhoneyServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace NotificationChannels\Phoney;
+
+use Illuminate\Support\ServiceProvider;
+
+class PhoneyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /*
+        $this->app->when(Channel::class)
+            ->needs(Pusher::class)
+            ->give(function () {
+                $pusherConfig = config('broadcasting.connections.pusher');
+
+                return new Pusher(
+                    $pusherConfig['key'],
+                    $pusherConfig['secret'],
+                    $pusherConfig['app_id']
+                );
+            });
+         */
+    }
+
+    /**
+     * Register the package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
