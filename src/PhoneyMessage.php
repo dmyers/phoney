06e3f66c684714e5fca6eb7ba3344075594a3cfe
Derek Myers <arcticpro@gmail.com>
2020-04-22 01:32:24 -0500

    cleanup

--- src/PhoneyMessage.php
+++ src/PhoneyMessage.php
@@ -4,5 +4,40 @@ namespace Dmyers\Phoney;
 
 class PhoneyMessage
 {
-    //
+    /**
+     * The text content of the message.
+     *
+     * @var string
+     */
+    public $body;
+
+    /**
+     * @param  string  $body
+     * @return static
+     */
+    public static function create($body = '')
+    {
+        return new static($body);
+    }
+
+    /**
+     * @param string $body
+     */
+    public function __construct($body = '')
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * Set the text content of the message.
+     *
+     * @param  string  $body
+     * @return $this
+     */
+    public function body($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
 }
