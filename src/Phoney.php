5cf9175cc7843c8afef3f4efb3781b0848b16612
Derek Myers <arcticpro@gmail.com>
2020-04-22 00:29:46 -0500

    cleanup

--- src/Phoney.php
+++ src/Phoney.php
@@ -4,13 +4,23 @@ namespace Dmyers\Phoney;
 
 class Phoney
 {
+    protected $data;
+
+    public function __construct()
+    {
+        $body = file_get_contents(__DIR__.'/../resources/sms.json');
+        $data = json_decode($body, true);
+
+        $this->data = $data;
+    }
+
     public function carriers()
     {
-
+        return $this->data;
     }
 
     public function gateways()
     {
-
+        return $this->data;
     }
 }
