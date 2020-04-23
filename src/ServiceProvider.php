ac14be17472f72e67fac0e3f823e3603705b9e14
Derek Myers <arcticpro@gmail.com>
2020-04-22 22:18:31 -0500

    cleanup

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,9 +6,9 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 /**
  * @see https://en.wikipedia.org/wiki/SMS_gateway
- * @see https://github.com/typpo/textbelt
  * @see https://github.com/brendanlim/sms-fu
  * @see https://github.com/preston/sms-easy
+ * @see https://github.com/typpo/textbelt
  * @see https://github.com/typpo/textbelt/blob/master/lib/carriers.js
  * @see https://github.com/typpo/textbelt/blob/master/lib/providers.js
  * @see https://github.com/mfitzp/List_of_SMS_gateways
