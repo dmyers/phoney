a62f7aff88e83eaca3f84442b53dbd6bcb9bd759
Derek Myers <arcticpro@gmail.com>
2020-04-22 22:21:08 -0500

    cleanup

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,12 +6,12 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 /**
  * @see https://en.wikipedia.org/wiki/SMS_gateway
+ * @see https://github.com/mfitzp/List_of_SMS_gateways
  * @see https://github.com/brendanlim/sms-fu
  * @see https://github.com/preston/sms-easy
  * @see https://github.com/typpo/textbelt
  * @see https://github.com/typpo/textbelt/blob/master/lib/carriers.js
  * @see https://github.com/typpo/textbelt/blob/master/lib/providers.js
- * @see https://github.com/mfitzp/List_of_SMS_gateways
  * @see https://github.com/laravel-notification-channels/twilio/tree/master/src
  * @see https://github.com/amnah/ksms
  */
