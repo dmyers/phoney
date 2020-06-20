825370bd3c1a9d5b6cbb7f3b3d722b89e15aba43
Derek Myers <arcticpro@gmail.com>
2020-06-20 00:24:37 -0500

    mailable

--- src/Mailable.php
+++ src/Mailable.php
@@ -2,9 +2,12 @@
 
 namespace Dmyers\Phoney;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable as BaseMailable;
+use Illuminate\Queue\SerializesModels;
 
 class Mailable extends BaseMailable
 {
-    //
+    use Queueable, SerializesModels;
 }
